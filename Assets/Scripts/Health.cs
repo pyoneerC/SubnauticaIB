45db4208144cda0b21626f6571d80e81675cef35
pyoneerC <maxcomperatutti@gmail.com>
2024-09-23 13:01:22 -0300

    feat: health kits

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
@@ -7,12 +9,15 @@ public class Health : MonoBehaviour
     public List<int> damageValues = new();
     private int _currentDamageIndex;
 
+    public Text healthText;
+
     private void Start()
     {
         damageValues.Add(50);
         damageValues.Add(30);
         damageValues.Add(19);
         damageValues.Add(1);
+        UpdateHealthUI();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,6 +26,10 @@ public class Health : MonoBehaviour
         {
             TakeDamage();
         }
+
+        if (!other.CompareTag("Kit") || !(health < 100f)) return;
+        StartCoroutine(Heal(5f));
+        Destroy(other.gameObject);
     }
 
     private void TakeDamage()
@@ -40,6 +49,8 @@ public class Health : MonoBehaviour
             {
                 Die();
             }
+
+            UpdateHealthUI();
         }
         else
         {
@@ -47,8 +58,33 @@ public class Health : MonoBehaviour
         }
     }
 
-    private static void Die()
+    private IEnumerator Heal(float duration)
     {
+        var targetHealth = Mathf.Min(health + 50f, 100f);
+        var healingAmount = targetHealth - health;
+        var healedAmountPerSecond = healingAmount / duration;
+        var timeElapsed = 0f;
 
+        while (timeElapsed < duration)
+        {
+            health += healedAmountPerSecond * Time.deltaTime;
+            health = Mathf.Max(health, 10f);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        health = Mathf.Clamp(health, 10f, 100f);
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (healthText == null) return;
+        healthText.text = (int)health + "%";
+    }
+
+    private void Die()
+    {
+        // Handle death logic here
     }
 }
